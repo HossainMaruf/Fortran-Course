9f0494b38185c515de29ea3383823f040dd6e08a
HossainMaruf <maruficepustian@gmail.com>
2025-08-14 17:18:49 +0600

    spelling mistake fixed

--- Operator/2. Arithmetic.f90
+++ Operator/2. Arithmetic.f90
@@ -1,4 +1,4 @@
-program Arithmatic
+program Arithmetic
     print *, 20+10     !addition
     print *, 20-10     !subtraction
     print *, 20*10     !multiplication
@@ -7,4 +7,4 @@ program Arithmatic
     print *, 1.0/3     !division
     print *, 1/3.0     !division
     print *, 1.0/3.0   !division
-end program Arithmatic
+end program Arithmetic
